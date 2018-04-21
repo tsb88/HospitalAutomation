60cab0159e2e29f007241495223f09fb7b4b0414
tsb88 <tejasbhoir23@gmail.com>
2018-04-21 15:07:10 -0400

    Firebase add

--- app/src/main/java/com/example/tejasbhoir/hospitalautomation/MainActivity.java
+++ app/src/main/java/com/example/tejasbhoir/hospitalautomation/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.tejasbhoir.hospitalautomation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference();
+        List<Staff> = new ArrayList<>();
+
+
+        EditText ID = findViewById(R.id.ID);
+        EditText password = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+
+        String id_input = ID.getText().toString();
+        String pass = password.getText().toString();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
