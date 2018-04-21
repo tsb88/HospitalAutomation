21bcecc73d7b39f31bc1ba517d0316523629017f
tsb88 <tejasbhoir23@gmail.com>
2018-04-21 15:48:14 -0400

    Login Added

--- app/src/main/java/com/example/tejasbhoir/hospitalautomation/MainActivity.java
+++ app/src/main/java/com/example/tejasbhoir/hospitalautomation/MainActivity.java
@@ -7,37 +7,124 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = db.getReference();
+    List<Staff> staffList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = db.getReference();
-        List<Staff> = new ArrayList<>();
 
 
         EditText ID = findViewById(R.id.ID);
         EditText password = findViewById(R.id.password);
         Button login = findViewById(R.id.login);
 
-        String id_input = ID.getText().toString();
-        String pass = password.getText().toString();
+        final String id_input = ID.getText().toString();
+        final String pass_input = password.getText().toString();
+
+        checkDatabaseReference();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                int size = staffList.size();
+                for (int i = 0; i < size; i++) {
+                    Staff staff = staffList.get(i);
+                    String password = staff.getPassword();
+                    String ID = staff.getID();
+                    if (id_input == ID) {
+                        if (pass_input == password) {
+                            if (staff.getClass() == Doctor.class) {
+                                // Start Doctor Activity
+                            }
+                            else {
+                                // Start Other Staff Activity
+                            }
+                        }
+                    }
+                }
             }
         });
 
     }
+
+    public void checkDatabaseReference() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild("staff")) {
+                    if (dataSnapshot.child("staff").hasChild("doctors")) {
+                        getDoctorList();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild("staff")){
+                    if(dataSnapshot.child("staff").hasChild("other")){
+                        getOtherList();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+        public void getDoctorList() {
+            myRef.child("staff").child("doctors").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        staffList.add(postSnapshot.getValue(Staff.class));
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        public void getOtherList() {
+            myRef.child("staff").child("other").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        staffList.add(postSnapshot.getValue(Staff.class));
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
 }
+
